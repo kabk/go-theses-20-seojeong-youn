46fc6efc8068cc7adb8870c2b3dddab87baae887
Seojeong Youn <lyricyoon@gmail.com>
2020-01-19 18:33:50 +0100

    a

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ $('a[href^="#"]').click(function () {
 
     return false;
 });
+
+
+$(function() {
+    var rotation = 0,
+        scrollLoc = $(document).scrollTop();
+    $(window).scroll(function() {
+        var newLoc = $(document).scrollTop();
+        var diff = scrollLoc - newLoc;
+        rotation += diff, scrollLoc = newLoc;
+        var rotationStr = "rotate(" + rotation + "deg)";
+        $(".arrow").css({
+            "-webkit-transform": rotationStr,
+            "-moz-transform": rotationStr,
+            "transform": rotationStr
+        });
+    });
+})
